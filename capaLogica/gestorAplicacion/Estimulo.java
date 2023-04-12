439c3b4734fb0b0f9f29b1dc974d5fe01163bc86
JPortoL <jporto@unal.edu.co>
2023-04-11 19:37:56 -0500

    Esqueleto Clases Horario y Estimulo

---
+++ capaLogica/gestorAplicacion/Estimulo.java
@@ -0,0 +1,27 @@
+package gestorAplicacion;
+
+import java.util.ArrayList;
+
+public class Estimulo {
+    private String nombre;
+    private String descripcion;
+    private tipoUsuario aQuienAplica;
+    private ArrayList<facultades> facultadesAplica;
+    private int cupos;
+    private int PBM;
+    private int PAPA;
+    private ArrayList<Curso> materiasImpartidas;
+    private ArrayList<tipoUsuario> inscritos;
+
+    //Constructores
+    public Estimulo(){
+        
+    }
+
+    //Metodos
+    public boolean verificarRequisitos(){
+
+    }
+
+    //Gets y Sets
+}
---
+++ capaLogica/gestorAplicacion/Horario.java
@@ -0,0 +1,25 @@
+package gestorAplicacion;
+
+import java.util.ArrayList;
+
+public class Horario {
+    private int id;
+    private Estudiante estudiante;
+    private ArrayList<Curso> cursos;
+
+    //Constructores
+    public Horario(){
+
+    }
+
+    //Metodos
+    public void mostrar(){
+
+    }
+
+    public Boolean validarDisponibilidad(){
+
+    }
+
+    //Sets y Gets
+}
