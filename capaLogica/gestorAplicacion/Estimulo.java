3d182bd9f232a92d1629eef1b1644d93f57ce904
JPortoL <jporto@unal.edu.co>
2023-04-17 23:13:20 -0500

    avance Estimulo y Horario
    Avances en las clases Estimulo y Horario, sobretodo en sus metodos

--- capaLogica/gestorAplicacion/Estimulo.java
+++ capaLogica/gestorAplicacion/Estimulo.java
@@ -2,26 +2,128 @@ package gestorAplicacion;
 
 import java.util.ArrayList;
 
+import javax.lang.model.util.ElementScanner6;
+
 public class Estimulo {
     private String nombre;
     private String descripcion;
     private tipoUsuario aQuienAplica;
-    private ArrayList<facultades> facultadesAplica;
+    private ArrayList<facultades> facultadesAplica = new ArrayList<>;
     private int cupos;
     private int PBM;
     private int PAPA;
     private ArrayList<Curso> materiasImpartidas;
-    private ArrayList<tipoUsuario> inscritos;
+    private ArrayList<tipoUsuario> inscritos = new ArrayList<>;
 
     //Constructores
-    public Estimulo(){
-        
+    public Estimulo(String nombre, String descripcion, tipoUsuario aQuienAplica, 
+    ArrayList<facultades> facultadesAplica, int cupos, int PBM, int PAPA, 
+    ArrayList<Curso> materiasImpartidas, ArrayList<tipoUsuario> inscritos) {
+        this.nombre = nombre;
+        this.descripcion = descripcion;
+        this.aQuienAplica = aQuienAplica;
+        this.facultadesAplica = facultadesAplica;
+        this.cupos = cupos;
+        this.PBM = PBM;
+        this.PAPA = PAPA;
+        this.materiasImpartidas = materiasImpartidas;
+        this.inscritos = inscritos;
     }
 
     //Metodos
-    public boolean verificarRequisitos(){
-
+    public boolean verificarRequisitos(Estudiante estudiante) {
+        ArrayList<String> razones = new ArrayList<>(); 
+        boolean cumpleRequisitos = true; 
+    
+        if (this.cupos <= 0) {
+            razones.add("Ya no quedan cupos para este estímulo");
+            cumpleRequisitos = false; 
+        }
+    
+        if (!this.facultadesAplica.contains(estudiante.getFacultad())) {
+            razones.add("Este estímulo no aplica a tu facultad");
+            cumpleRequisitos = false; 
+        }
+    
+        if (estudiante.calcularPAPA() < this.PAPA) {
+            razones.add("Tu PAPA es demasiado bajo");
+            cumpleRequisitos = false; 
+    
+        if (cumpleRequisitos) {
+            System.out.println("Cumples con todos los requisitos para inscribirte");
+            return true;
+        } else {
+            System.out.println("No puedes aplicar a este estímulo por las siguientes razones:");
+            for (String razon : razones) {
+                System.out.println(razon);
+            }
+            return false;
+        }
     }
+    
 
     //Gets y Sets
+
+    public String getNombre() {
+        return nombre;
+    }
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+    
+    public String getDescripcion() {
+        return descripcion;
+    }
+    public void setDescripcion(String descripcion) {
+        this.descripcion = descripcion;
+    }
+    
+    public tipoUsuario getaQuienAplica() {
+        return aQuienAplica;
+    }
+    public void setaQuienAplica(tipoUsuario aQuienAplica) {
+        this.aQuienAplica = aQuienAplica;
+    }
+
+    public ArrayList<facultades> getFacultadesAplica() {
+        return facultadesAplica;
+    }
+    public void setFacultadesAplica(ArrayList<facultades> facultadesAplica) {
+        this.facultadesAplica = facultadesAplica;
+    }
+    
+    public int getCupos() {
+        return cupos;
+    }
+    public void setCupos(int cupos) {
+        this.cupos = cupos;
+    }
+    
+    public int getPBM() {
+        return PBM;
+    }
+    public void setPBM(int PBM) {
+        this.PBM = PBM;
+    }
+    
+    public int getPAPA() {
+        return PAPA;
+    }
+    public void setPAPA(int PAPA) {
+        this.PAPA = PAPA;
+    }
+    
+    public ArrayList<Curso> getMateriasImpartidas() {
+        return materiasImpartidas;
+    }
+    public void setMateriasImpartidas(ArrayList<Curso> materiasImpartidas) {
+        this.materiasImpartidas = materiasImpartidas;
+    }
+    
+    public ArrayList<tipoUsuario> getInscritos() {
+        return inscritos;
+    }
+    public void setInscritos(ArrayList<tipoUsuario> inscritos) {
+        this.inscritos = inscritos;
+    }
 }
--- capaLogica/gestorAplicacion/Horario.java
+++ capaLogica/gestorAplicacion/Horario.java
@@ -5,21 +5,70 @@ import java.util.ArrayList;
 public class Horario {
     private int id;
     private Estudiante estudiante;
-    private ArrayList<Curso> cursos;
+    private ArrayList<Curso> cursos = new ArrayList<>;
+    private static int count;
 
     //Constructores
-    public Horario(){
+    public Horario(Estudiante estudiante, ArrayList<Curso> cursos){
+        this.estudiante = estudiante;
+        this.cursos = cursos;
+        Horario.count++;
+        this.id = Horario.count;
+    }
 
+    public Horario(int id, Estudiante estudiante, ArrayList<Curso> cursos){
+        this.estudiante = estudiante;
+        this.cursos = cursos;
+        this.id = id;
     }
 
     //Metodos
-    public void mostrar(){
 
+    public void mostrar(){
+        // Provicional mientras se define bien la clase Curso
+        for (Curso curso : this.cursos) {
+            System.out.println("-----------------------------------------");
+            System.out.println(curso.getNombre() + ": " + curso.getHorariosClase());
+        }
+        System.out.println("-----------------------------------------");
     }
+    
 
     public Boolean validarDisponibilidad(){
-
+        for (Curso curso1 : this.cursos){
+            for (String horario1 : curso1.getHorariosClase()){
+                for (Curso curso2 : this.cursos){
+                    for (String horario2 : curso1.getHorariosClase()){
+                        if (horario1 == horario2){
+                            return false;
+                        }
+                    }
+                }
+            }
+            
+        }
+        return true;
     }
 
     //Sets y Gets
-}
+    public int getId() {
+        return id;
+    }
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public Estudiante getEstudiante() {
+        return estudiante;
+    }
+    public void setEstudiante(Estudiante estudiante) {
+        this.estudiante = estudiante;
+    }
+
+    public ArrayList<Curso> getCursos() {
+        return cursos;
+    }
+    public void setCursos(ArrayList<Curso> cursos) {
+        this.cursos = cursos;
+    }
+    }
