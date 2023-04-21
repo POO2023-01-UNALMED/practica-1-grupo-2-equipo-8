403cf6921115de063995d702705f0b1a0fd2a584
Sarestrepoag <samuelaguilar.1235@gmail.com>
2023-04-20 19:51:29 -0500

    Correción (No subí el serializador ni el deserializador)

---
+++ src/capaPersistencia/Deserializador.java
@@ -0,0 +1,93 @@
+package capaPersistencia;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.FileInputStream;
+import java.io.ObjectInputStream;
+import gestorAplicacion.Registro;
+import gestorAplicacion.Profesor;
+import gestorAplicacion.Estudiante;
+import gestorAplicacion.Admin;
+import gestorAplicacion.Curso;
+import java.util.ArrayList;
+
+public class Deserializador {
+    private static File temp = new File("src\\capaPersistencia\\temp");
+    public static void deserializador(){
+        File[] docs = temp.listFiles();
+        FileInputStream fis;
+        ObjectInputStream ois;
+        
+        for(File f : docs){
+            if(f.getAbsolutePath().contains("profesores")){
+                try{
+                    fis = new FileInputStream(f);
+                    ois = new ObjectInputStream(fis);
+                    
+                    Registro.setProfesores((ArrayList<Profesor>) ois.readObject());
+                }
+                catch (FileNotFoundException e){
+                    e.printStackTrace();
+                }
+                catch (IOException e){
+                    e.printStackTrace();
+                }
+                catch (ClassNotFoundException e) {
+                    e.printStackTrace();
+                }
+            }
+            else if(f.getAbsolutePath().contains("estudiantes")){
+                try{
+                    fis = new FileInputStream(f);
+                    ois = new ObjectInputStream(fis);
+                    
+                    Registro.setEstudiantes((ArrayList<Estudiante>) ois.readObject());
+                }
+                catch (FileNotFoundException e){
+                    e.printStackTrace();
+                }
+                catch (IOException e){
+                    e.printStackTrace();
+                }
+                catch (ClassNotFoundException e) {
+                    e.printStackTrace();
+                }
+            }
+            else if(f.getAbsolutePath().contains("admins")){
+                try{
+                    fis = new FileInputStream(f);
+                    ois = new ObjectInputStream(fis);
+                    
+                    Registro.setAdmins((ArrayList<Admin>) ois.readObject());
+                }
+                catch (FileNotFoundException e){
+                    e.printStackTrace();
+                }
+                catch (IOException e){
+                    e.printStackTrace();
+                }
+                catch (ClassNotFoundException e) {
+                    e.printStackTrace();
+                }
+            }
+            else if(f.getAbsolutePath().contains("cursos")){
+                try{
+                    fis = new FileInputStream(f);
+                    ois = new ObjectInputStream(fis);
+                    
+                    Registro.setCursos((ArrayList<Curso>) ois.readObject());
+                }
+                catch (FileNotFoundException e){
+                    e.printStackTrace();
+                }
+                catch (IOException e){
+                    e.printStackTrace();
+                }
+                catch (ClassNotFoundException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+    }
+}
---
+++ src/capaPersistencia/Serializador.java
@@ -0,0 +1,99 @@
+package capaPersistencia;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.FileOutputStream;
+import java.io.ObjectOutputStream;
+import java.io.PrintWriter;
+import gestorAplicacion.Registro;
+
+public class Serializador {
+    private static File temp = new File("src\\capaPersistencia\\temp");
+    
+    public static void serializador(){
+        FileOutputStream fos;
+        ObjectOutputStream oos;
+        File[] docs = temp.listFiles();
+        PrintWriter pw;
+        
+        for(File f : docs){
+            
+            try{
+                pw = new PrintWriter(f);
+                pw.print("");
+            }
+            catch (FileNotFoundException e){
+                e.printStackTrace();
+            }      
+        }
+        for(File f : docs){
+            
+            if(f.getAbsolutePath().contains("profesores")){
+                try{
+                    fos = new FileOutputStream(f);
+                    oos = new ObjectOutputStream(fos);
+                    oos.writeObject(Registro.getProfesores());
+                }
+                catch (FileNotFoundException e){
+                    e.printStackTrace();
+                }
+                catch (IOException e){
+                    e.printStackTrace();
+                }
+            }
+            else if(f.getAbsolutePath().contains("estudiantes")){
+                try{
+                    fos = new FileOutputStream(f);
+                    oos = new ObjectOutputStream(fos);
+                    oos.writeObject(Registro.getEstudiantes());
+                }
+                catch (FileNotFoundException e){
+                    e.printStackTrace();
+                }
+                catch (IOException e){
+                    e.printStackTrace();
+                }
+            }
+            else if(f.getAbsolutePath().contains("admins")){
+                try{
+                    fos = new FileOutputStream(f);
+                    oos = new ObjectOutputStream(fos);
+                    oos.writeObject(Registro.getAdmins());
+                }
+                catch (FileNotFoundException e){
+                    e.printStackTrace();
+                }
+                catch (IOException e){
+                    e.printStackTrace();
+                }
+            }
+            else if(f.getAbsolutePath().contains("cursos")){
+                try{
+                    fos = new FileOutputStream(f);
+                    oos = new ObjectOutputStream(fos);
+                    oos.writeObject(Registro.getCursos());
+                }
+                catch (FileNotFoundException e){
+                    e.printStackTrace();
+                }
+                catch (IOException e){
+                    e.printStackTrace();
+                }
+            }
+            else if(f.getAbsolutePath().contains("estudiantesMatriculados")){
+                try{
+                    fos = new FileOutputStream(f);
+                    oos = new ObjectOutputStream(fos);
+                    oos.writeObject(Registro.getEstudiantesMatriculados());
+                }
+                catch (FileNotFoundException e){
+                    e.printStackTrace();
+                }
+                catch (IOException e){
+                    e.printStackTrace();
+                }
+            }
+        }
+    }
+}
