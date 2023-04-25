c13fcb6beec66f3dd3e0a3456ba95616f54255b1
jprm610 <jprm610@gmail.com>
2023-04-25 13:10:14 -0500

    0.22

--- src/gestorAplicacion/Admin.java
+++ src/gestorAplicacion/Admin.java
@@ -35,6 +35,7 @@ public class Admin extends Registro {
             listaPorcentajes.add(nota);
         }
 
+        // PREREQUISITOS
         // Se definen los cursos que serán prerrequisitos.
         ArrayList<Curso> preRequisitos = new ArrayList<Curso>();
         while (true) {
@@ -65,30 +66,66 @@ public class Admin extends Registro {
             preRequisitos.add(notInPreRequisitos.get(opcion - 1));
         }
 
+        // CARRERAS
+        // Se definen las carreras que están relacionadas con el curso.
         ArrayList<Carreras> carrerasRelacionadas = new ArrayList<Carreras>();
         while (true) {
-            for (int i = 0; i < Carreras.values().length; i++) {
-                System.out.printf("%d. %s\n", i + 1, Carreras.values()[i].getNombre());
+            // Se construye la lista de carreras que no están relacionadas,
+            // esto para evitar repeticiones.
+            ArrayList<Carreras> notIncarrerasRelacionadas = new ArrayList<Carreras>();
+            for (Carreras carrera : Carreras.values()) {
+                if (!carrerasRelacionadas.contains(carrera)) {
+                    notIncarrerasRelacionadas.add(carrera);
+                }
             }
-            System.out.print("Elige una carrera relacionada o 0 para continuar: ");
+            // Si no hay carreras disponibles, se continua al siguiente apartado.
+            if (notIncarrerasRelacionadas.isEmpty()) {
+                System.out.println("No hay carreras disponibles.");
+                break;
+            }
+
+            // Se imprimen las posibles carreras.
+            for (int i = 0; i < notIncarrerasRelacionadas.size(); i++) {
+                System.out.printf("%d. %s\n", i + 1, notIncarrerasRelacionadas.get(i).getNombre());
+            }
+            // El usuario elige alguna carrera o 0 para continuar con el siguiente apartado.
+            System.out.print("Elige una carrera o 0 para continuar: ");
             int opcion = sc.nextInt();
-            if (opcion == 0 || opcion > Carreras.values().length) break;
-            if (!carrerasRelacionadas.contains(Carreras.values()[opcion - 1])) {
-                carrerasRelacionadas.add(Carreras.values()[opcion - 1]);
-            }
+            if (opcion == 0 || opcion > notIncarrerasRelacionadas.size()) break;
+
+            // Se agrega la carrera seleccionada como relacionada.
+            carrerasRelacionadas.add(notIncarrerasRelacionadas.get(opcion - 1));
         }
 
+        // PROFESORES
+        // Se definen los profesores que dictan el curso.
         ArrayList<Profesor> profesoresQueDictanElCurso = new ArrayList<Profesor>();
         while (true) {
-            for (int i = 0; i < Registro.getProfesores().size(); i++) {
-                System.out.printf("%d. %s\n", i + 1, Registro.getProfesores().get(i).getNombre());
+            // Se construye la lista de profesores que no dictan el curso,
+            // esto para evitar repeticiones.
+            ArrayList<Profesor> notInProfesoresQueDictanElCurso = new ArrayList<Profesor>();
+            for (Profesor profesor : Registro.getProfesores()) {
+                if (!profesoresQueDictanElCurso.contains(profesor)) {
+                    notInProfesoresQueDictanElCurso.add(profesor);
+                }
             }
-            System.out.print("Elige un profesor que dicta el curso o 0 para continuar: ");
+            // Si no hay profesores disponibles, se continua al siguiente apartado.
+            if (notInProfesoresQueDictanElCurso.isEmpty()) {
+                System.out.println("No hay profesores disponibles.");
+                break;
+            }
+
+            // Se imprimen los profesores disponibles.
+            for (int i = 0; i < notInProfesoresQueDictanElCurso.size(); i++) {
+                System.out.printf("%d. %s\n", i + 1, notInProfesoresQueDictanElCurso.get(i).getNombre());
+            }
+            // El usuario elige algún profesor o 0 para continuar con el siguiente apartado.
+            System.out.print("Elige un profesor o 0 para continuar: ");
             int opcion = sc.nextInt();
-            if (opcion == 0 || opcion > Registro.getProfesores().size()) break;
-            if (!profesoresQueDictanElCurso.contains(Registro.getProfesores().get(opcion - 1))) {
-                profesoresQueDictanElCurso.add(Registro.getProfesores().get(opcion - 1));
-            }
+            if (opcion == 0 || opcion > notInProfesoresQueDictanElCurso.size()) break;
+
+            // Se agrega el profesor seleccionado.
+            profesoresQueDictanElCurso.add(notInProfesoresQueDictanElCurso.get(opcion - 1));
         }
 
         ArrayList<Facultades> facultades = new ArrayList<Facultades>();
