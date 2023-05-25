2c541b98a7875a5d9d25bcf1059797271b4f65b1
jprm610 <jprm610@gmail.com>
2023-05-24 21:28:17 -0500

    Admin

--- src/uiMain/Menu.java
+++ src/uiMain/Menu.java
@@ -225,9 +225,9 @@ public class Menu {
                     + "10. Salir");
             String opcion = sc.nextLine();
             ArrayList<String> opciones = new ArrayList<String>(
-                    Arrays.asList("1", "2", "3", "4", "5", "6", "7", "8", "9"));
+                    Arrays.asList("1", "2", "3", "4", "5", "6", "7", "8", "9","10"));
             if (!opciones.contains(opcion)) {
-                System.out.println("Debe seleccionar un número entre el 1 y el 9");
+                System.out.println("Debe seleccionar un número entre el 1 y el 10");
                 continue;
             }
             switch (opcion) {
