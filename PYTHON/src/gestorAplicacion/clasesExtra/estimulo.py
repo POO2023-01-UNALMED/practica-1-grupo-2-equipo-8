248bb568a13ffe06016d999f0481c4dc466e5fa9
JPortoL <jporto@unal.edu.co>
2023-06-12 14:56:17 -0500

    Create estimulo.py

---
+++ PYTHON/src/gestorAplicacion/clasesExtra/estimulo.py
@@ -0,0 +1,50 @@
+import pickle
+
+class Estimulo:
+    def __init__(self, nombre, descripcion, aQuienAplica, facultadesAplica, cupos):
+        self.nombre = nombre
+        self.descripcion = descripcion
+        self.aQuienAplica = aQuienAplica
+        self.facultadesAplica = facultadesAplica
+        self.cupos = cupos
+
+    def getAQuienAplica(self):
+        return self.aQuienAplica
+
+    def getCupos(self):
+        return self.cupos
+
+    def getDescripcion(self):
+        return self.descripcion
+
+    def getFacultadesAplica(self):
+        return self.facultadesAplica
+
+    def getNombre(self):
+        return self.nombre
+
+    def setAQuienAplica(self, aQuienAplica):
+        self.aQuienAplica = aQuienAplica
+
+    def setCupos(self, cupos):
+        self.cupos = cupos
+
+    def setDescripcion(self, descripcion):
+        self.descripcion = descripcion
+
+    def setFacultadesAplica(self, facultadesAplica):
+        self.facultadesAplica = facultadesAplica
+
+    def setNombre(self, nombre):
+        self.nombre = nombre
+
+# Serialización
+def serializar(objeto, archivo):
+    with open(archivo, 'wb') as file:
+        pickle.dump(objeto, file)
+
+# Deserialización
+def deserializar(archivo):
+    with open(archivo, 'rb') as file:
+        objeto = pickle.load(file)
+    return objeto
