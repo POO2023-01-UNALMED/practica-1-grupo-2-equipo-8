7cc765d55f385f5bded845a2dd38d1416d7e7a41
jprm610 <jprm610@gmail.com>
2023-06-15 15:54:00 -0500

    SOLUCIONLOGIN

--- PYTHON/src/gestorGrafico/UserWindow.py
+++ PYTHON/src/gestorGrafico/UserWindow.py
@@ -1,11 +1,26 @@
-from tkinter import Tk, Frame
+from tkinter import Tk, Frame, Button, LEFT
 
 class UserWindow :
     def __init__(self) -> None:
         window = Tk()
         window.title('Mi Gestor Académico')
-        window.geometry("800x400")
+        window.state("zoomed")
 
-        frame1 = Frame()
+        frameBotones = Frame(window)
+        
+        frameBotones.columnconfigure(0, weight=1)
+        frameBotones.columnconfigure(1, weight=1)
+        frameBotones.columnconfigure(2, weight=1)
+
+        botonArchivo = Button(frameBotones, text="Archivo", font=("Arial", 18))
+        botonArchivo.grid(row='1', column='0')
+
+        botonProcesos = Button(frameBotones, text="Procesos y Consultas", font=("Arial", 18))
+        botonProcesos.grid(row='1', column='1')
+
+        botonAyuda = Button(frameBotones, text="Ayuda", font=("Arial", 18))
+        botonAyuda.grid(row='1', column='2')
+
+        frameBotones.pack(side=LEFT)
 
         window.mainloop()
--- PYTHON/src/main.py
+++ PYTHON/src/main.py
@@ -2,5 +2,5 @@ from gestorGrafico.UserWindow import UserWindow
 from gestorGrafico.Login import Login
 
 if __name__ == '__main__' :
-    Login()
+    Login().iniciar()
     #UserWindow()
