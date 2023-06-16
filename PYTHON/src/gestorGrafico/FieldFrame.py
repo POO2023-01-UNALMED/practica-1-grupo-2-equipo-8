9f7a9057f9f019f7ff890181ab8428f303357091
jprm610 <jprm610@gmail.com>
2023-06-16 00:34:13 -0500

    User 0.1

--- PYTHON/src/gestorGrafico/FieldFrame.py
+++ PYTHON/src/gestorGrafico/FieldFrame.py
@@ -1,7 +1,16 @@
-from tkinter import Frame
+import tkinter as tk
 
-class FieldFrame(Frame) :
-    def __init__(self, ventana, titulosCriterios, criterios, titulosValores, valores = None) :
-        super.__init__(ventana)
+class FieldFrame(tk.Frame):
+    def __init__(self, ventana, tituloProceso=None, descripcionProceso=None, titulosCriterios=None, criterios=None, titulosValores=None, valores = None) :
+        super().__init__(ventana)
 
-        
+        self._tituloProceso = tituloProceso
+        self._descripcionProceso = descripcionProceso
+        self._titulosCriterios = titulosCriterios
+        self._criterios = criterios
+        self._titulosValores = titulosValores
+        self._valores = valores
+
+        proceso = tk.Label(self, text=self._tituloProceso)
+        proceso.place(anchor=tk.CENTER)
+        proceso.pack()
--- PYTHON/src/gestorGrafico/UserWindow.py
+++ PYTHON/src/gestorGrafico/UserWindow.py
@@ -1,4 +1,5 @@
-from tkinter import Tk, Frame, Button, LEFT
+from tkinter import Tk, Frame, Button, Menu, LEFT
+from .FieldFrame import FieldFrame
 
 class UserWindow :
     def __init__(self) -> None:
@@ -6,21 +7,56 @@ class UserWindow :
         window.title('Mi Gestor Académico')
         window.state("zoomed")
 
+        barra_menus = Menu()
+        menu_archivo = Menu(barra_menus, tearoff=False)
+        menu_archivo.add_command(
+            label="Nuevo",
+            accelerator="Ctrl+N",
+        )
+        window.bind_all("<Control-n>")
+        menu_archivo.add_separator()
+        menu_archivo.add_command(label="Salir", command=window.destroy)
+        menu_opciones = Menu(barra_menus, tearoff=False)
+        barra_menus.add_cascade(menu=menu_archivo, label="Archivo")
+        barra_menus.add_cascade(menu=menu_opciones, label="Opciones")
+        window.config(menu=barra_menus)
+        window.mainloop()
+
+        # Menu Superior
         frameBotones = Frame(window)
-        
+        frameBotones.grid(row=0, column=0, sticky='ew')
+
         frameBotones.columnconfigure(0, weight=1)
         frameBotones.columnconfigure(1, weight=1)
         frameBotones.columnconfigure(2, weight=1)
 
-        botonArchivo = Button(frameBotones, text="Archivo", font=("Arial", 18))
-        botonArchivo.grid(row='1', column='0')
+        bar = Menu(window)
+
+        botonArchivo = Menu(bar)
+        botonArchivo.add_cascade(label='Aplicacion')
+        botonArchivo.add_cascade(label='Salir')
 
         botonProcesos = Button(frameBotones, text="Procesos y Consultas", font=("Arial", 18))
-        botonProcesos.grid(row='1', column='1')
+        botonProcesos.grid(row=1, column=1)
 
         botonAyuda = Button(frameBotones, text="Ayuda", font=("Arial", 18))
-        botonAyuda.grid(row='1', column='2')
+        botonAyuda.grid(row=1, column=2)
 
-        frameBotones.pack(side=LEFT)
+        # Interaccion Usuario
+        frameInteraccion = FieldFrame(window, 'A')
+        frameInteraccion.grid(row=1, column=0, sticky='nsew')
+
+        
+        # Configure grid weights to allow frames to expand
+        window.grid_rowconfigure(0, weight=1)
+        window.grid_rowconfigure(1, weight=1)
+        window.grid_columnconfigure(0, weight=1)
 
         window.mainloop()
+
+    @classmethod
+    def handleArchivo() :
+        pass
+
+    def handleProcesos() :
+        pass
